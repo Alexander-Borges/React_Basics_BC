ad16ba2ffbeb0d6dcce2fb98ba663a6f6f2e4e9e
Alexander Borges <borges2721@gmail.com>
2024-06-09 16:24:09 -0400

    Add files via upload

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+function Food(){
+
+    const food1 = "Burger";
+    const food2 = "Taco";
+
+    return(
+        <ul>
+            <li>Birria</li>
+            <li></li>
+            <li></li>
+            
+        </ul>
+    );
+}
+
+export default Food
